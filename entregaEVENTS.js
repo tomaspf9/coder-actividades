aa3df46a1d707f4ac0dfe0e9cdd2f7cd95259312
tomaspf9 <tomaspf9@gmail.com>
2022-09-22 16:04:02 -0300

    Dark Mode toggle activado

--- entregaEVENTS.js
+++ entregaEVENTS.js
@@ -1,8 +1,20 @@
-/// Entrega de eventos... 
 
-// Cree unas variables en algunos botones, también se creo una función para que me devuelva un texto
-// cuando clickee en el específico boton
-
+const listaCanciones = [
+    {
+        tittle: "Why'd you only call me when you're high",
+        file: "articmonkeys1.mp3",
+        artist: "Artic Monkeys"    },
+    {   
+        tittle: "How you remind me",
+        file: "nickelback1",
+        artist: "Niclelback"
+    },
+    {
+        tittle: "Dark Necessities",
+        file: "rhcp1",
+        artis: "Red Hot Chilli Peppers"
+    }    
+]
 
 let buscador = document.querySelector(".btn")
 buscador.addEventListener("click",() => {
@@ -10,9 +22,9 @@ buscador.addEventListener("click",() => {
 })
 
 
-    let masRock = document.querySelector("#btnRock")
+    let masRock = document.querySelector(".play")
     masRock.addEventListener("click",() =>{
-        alert("Elemento en mantenimiento!")
+        
 })
 
 let cuenta = document.querySelector(".nav-link.dropdown-toggle")
@@ -22,3 +34,31 @@ cuenta.onclick = alertRegistro
 function alertRegistro(){
     cuenta.innerText = 'Regístrate!'
 }
+// Darkmode activado/desactivado
+// Nos guiamos del localstorage y las preferencias del usuario para que queden guardadas
+let darkMode = localStorage.getItem('darkMode')
+const darkModeToggle =document.querySelector('#bDark')
+
+
+const activarDarkMode = () =>{
+    document.body.classList.add('darkmode')
+
+    localStorage.setItem('darkMode', 'activado')
+}
+const desactivarDarkMode = () =>{
+    document.body.classList.remove('darkmode')
+
+    localStorage.setItem('darkMode', 'desactivado')
+}
+if(darkMode == 'activado'){
+    activarDarkMode()
+}
+darkModeToggle.addEventListener('click',()=>{
+    darkMode = localStorage.getItem('darkMode')
+    if (darkMode !== 'activado'){
+        activarDarkMode()
+    } else{
+        desactivarDarkMode()
+    }
+    
+})
