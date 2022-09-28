4ffa974b98d3bd776afd1a7247caf29514fe7e9f
tomaspf9 <84751753+tomaspf9@users.noreply.github.com>
2022-09-27 21:26:57 -0300

    operadores avanzados en practica

---
+++ operadoresAvanzados.js
@@ -0,0 +1,28 @@
+// Spread
+
+const listaMetal = ['Metallica', 'System of a Down']
+const listaRock = ['RHCP', 'Nickelback', 'Artic Monkeys']
+
+const listaMusical = [...listaMetal,...listaRock]
+
+console.log(listaMusical)
+
+const listaMusicalObjecto = {
+    ...listaMusical
+}
+console.log(listaMusicalObjecto)
+
+
+//  
+const artistaAM = {
+        tittle: "Why'd you only call me when you're high",
+        file: "articmonkeys1.mp3",
+        artist: "Artic Monkeys"  
+    }
+   
+//
+console.log(artistaAM?.file || "No se encuentra dicho elemento")
+
+console.log(artistaAM?.canciones || "No se encuentra dicho elemento")
+
+
